c2f71da1370c5edc33c59423a485cbc1f9d23432
Ambroise <ambroise.houndonougbo@gmail.com>
2020-08-03 21:44:09 +0200

    la notion de varaible en php

---
+++ variables/variable.php
@@ -0,0 +1,15 @@
+<?php 
+
+/***
+ * int entier
+ * float decimal reél
+ * string chaine de caractères ou caractères
+ * bool boolean
+ * 
+ * array tableau
+ * object object
+ */
+
+$mavariable = 5 ;
+$mavariable = 10;
+echo $mavariable;
